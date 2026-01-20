952f2073604eacfb246016fc4c022367f406da3a
Pogbe Emmanuel <mauyonpogbe@gmail.com>
2026-01-20 14:34:11 +0100

    Changed port settings and added procfile

--- app.py
+++ app.py
@@ -200,7 +200,8 @@ if __name__ == '__main__':
         print("="*80 + "\n")
         
         # Run the Flask app
-        app.run(debug=True, host='0.0.0.0', port=5000)
+        port = int(os.environ.get("PORT",5000))
+        app.run(debug=True, host='0.0.0.0', port=port)
     else:
         print("\n✗ Failed to load model components. Please check the files.")
         print("  Required files:")
